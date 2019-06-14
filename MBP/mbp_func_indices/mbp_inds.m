f18b2be0c2d762087b356cde4a1288f3e59ef4ec
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-14 10:26:27 -0400

    Changed inds_mat size to account for a and b values

--- MBP/mbp_func_indices/mbp_inds.m
+++ MBP/mbp_func_indices/mbp_inds.m
@@ -6,14 +6,14 @@ for ct1=0:N
         tot_num = tot_num + (2*ct1+1)*(2*ct2+1)*(2*ct3+1);
     end
 end
-tot_inds = zeros(tot_num,5);
+tot_inds = zeros(tot_num,7);
 ind_start = 1;
 for ct1=0:N
     for ct2=0:N
         inds_mat = mbp_ab(ct1,ct2);
         ind_stop = ind_start+size(inds_mat,1)-1;
         tot_inds(ind_start:ind_stop,1) = ind_start:ind_stop;
-        tot_inds(ind_start:ind_stop,2:5) = inds_mat;
+        tot_inds(ind_start:ind_stop,2:7) = inds_mat;
         ind_start = ind_stop+1;
     end
 end
