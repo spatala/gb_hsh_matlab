3c536457e8b622ae507b06951e6b4f03723486c5
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-14 09:39:11 -0400

    Added code for ordering indices for MBP functions.

---
+++ MBP/mbp_func_indices/mbp_ab.m
@@ -0,0 +1,16 @@
+function inds_mat = mbp_ab(a,b)
+c = min(a,b);
+tot_num = (2*a+1)*(2*b+1)*(2*c+1);
+inds1 = 1:tot_num;
+ct1 = 1;
+inds_mat = zeros(tot_num,4);
+for g1=-c:c
+    for a1=-a:a
+        for b1=-b:b
+            inds_mat(ct1, :) = [inds1(ct1), g1, a1, b1];
+            ct1=ct1+1;
+        end
+    end
+end
+
+end
