359d40ce774f71a83c377b6899e8b23f6e7d2229
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-17 14:41:23 -0400

    Modified check_ges.m to work for array of values for N.

--- GES/check_ges.m
+++ GES/check_ges.m
@@ -1,6 +1,9 @@
 clear all; clc;
 
-clear all; clc;
+addpath(genpath('../Util_functions/'));
+addpath(genpath('../GB_Parameters/'));
+
+
 s1 = load('rand_gb_rots.mat');
 rot_mats = s1.rot_mats;
 
@@ -9,12 +12,14 @@ Svec = s1.S;
 
 rots = rot_mats(:,:,1);
 
-r1 = rots(:,1:3); r2 = rots(:,4:6);
-N = 1;
-M1 = mbp_funcs_vals(r1, r2, N);
+N = [0,4];
+M1 = mbp_funcs_vals(rots, N);
 
 g_ypi = vrrotvec2mat([0,1,0, pi]);
 r1_ges = g_ypi*r2; r2_ges = g_ypi*r1;
-M1_ges = mbp_funcs_vals(r1_ges, r2_ges, N);
+M1_ges = mbp_funcs_vals([r1_ges, r2_ges], N);
 
 norm(M1'*Svec - M1_ges'*Svec)
+
+rmpath(genpath('../Util_functions/'));
+rmpath(genpath('../GB_Parameters/'));
