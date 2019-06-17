b863920fe48f0a5991e53d46d1b237284d66cab1
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-17 12:09:33 -0400

    Commented mbp_ab function

--- MBP/mbp_ab.m
+++ MBP/mbp_ab.m
@@ -1,4 +1,22 @@
 function inds_mat = mbp_ab(a,b)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%% Get indices for MBP functions with fixed a and b.
+%%%%%%%
+%%%%%%% Input
+%%%%%%% a, b: Integers
+%%%%%%%         order of MBP function (gamma_MBP^{a,b}_{alpha, beta}
+%%%%%%%
+%%%%%%% Output
+%%%%%%% mbp: N X 6 matrix.
+%%%%%%%         The columns are as follows:
+%%%%%%%         1) Index number for fixed a, b.
+%%%%%%%         2) Value of 'a'
+%%%%%%%         3) Value of 'b'
+%%%%%%%         4) Value of 'gamma' - range [-c, c], where $c = min(a,b)$
+%%%%%%%         5) Value of 'alpha' - range [-a, a]
+%%%%%%%         6) Value of 'beta'  - range [-b, b]
+%%%%%%%
+
 c = min(a,b);
 tot_num = (2*a+1)*(2*b+1)*(2*c+1);
 inds1 = 1:tot_num;
