0c9d4e625ee822307222a473dc263c94245d48b8
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-17 14:42:57 -0400

    Changed mbp_func_vals.m to work for array of values for N.

--- Util_functions/mbp_funcs_vals.m
+++ Util_functions/mbp_funcs_vals.m
@@ -6,81 +6,66 @@ function M_vals = mbp_funcs_vals(rots, N)
 %%%%%%% N: Integer
 %%%%%%%         The maximum order for MBP functions.
 %%%%%%%         We impose a = b = N.
+%%%%%%%  : 1-D Array
+%%%%%%%         The possible values of $a, b$ are provided.
 %%%%%%%
-%%%%%%% Input
 %%%%%%% rots: 3 X 6 matrix
 %%%%%%%         First three columns are the first orientation matrix (g1)
 %%%%%%%         and the second three are the second orientaiton matrix (g2)
 %%%%%%%
 %%%%%%% Output
 %%%%%%% M_vals: 1 X N row vector.
-%%%%%%%         The value of the function MBP function 
+%%%%%%%         The value of the function MBP function
 %%%%%%%         (gamma_MBP^{a,b}_{alpha, beta})
 %%%%%%%         for different values of a, b, gamma, alpha, beta
 %%%%%%%         are calculated for the GB parameter (given by rot)
 %%%%%%%
 
-r1=rots(:,1:3); r2=rots(:,4:6);
-
 tot_num = 0;
-for ct1=0:N
-    for ct2=0:N
-        ct3 = min(ct1, ct2);
-        tot_num = tot_num + (2*ct1+1)*(2*ct2+1)*(2*ct3+1);
+
+if (length(N) == 1)
+    for ct1=0:N
+        for ct2=0:N
+            ct3 = min(ct1, ct2);
+            tot_num = tot_num + (2*ct1+1)*(2*ct2+1)*(2*ct3+1);
+        end
+    end
+    tot_inds = zeros(tot_num,7);
+    M_vals = zeros(1,tot_num);
+    ind_start = 1;
+    for ct1=0:N
+        for ct2=0:N
+            M = calc_Mfunc(ct1,ct2,rots);
+            inds_mat = mbp_ab(ct1,ct2);
+            ind_stop = ind_start+size(inds_mat,1)-1;
+            tot_inds(ind_start:ind_stop,1) = ind_start:ind_stop;
+            tot_inds(ind_start:ind_stop,2:7) = inds_mat;
+            M_vals(ind_start:ind_stop,1) = M';
+            ind_start = ind_stop+1;
+        end
+    end
+elseif (length(N) == 2)
+    for ct1=N
+        for ct2=N
+            ct3 = min(ct1, ct2);
+            tot_num = tot_num + (2*ct1+1)*(2*ct2+1)*(2*ct3+1);
+        end
+    end
+    tot_inds = zeros(tot_num,7);
+    M_vals = zeros(1,tot_num);
+    ind_start = 1;
+    for ct1=N
+        for ct2=N
+            M = calc_Mfunc(ct1,ct2,rots);
+            inds_mat = mbp_ab(ct1,ct2);
+            ind_stop = ind_start+size(inds_mat,1)-1;
+            tot_inds(ind_start:ind_stop,1) = ind_start:ind_stop;
+            tot_inds(ind_start:ind_stop,2:7) = inds_mat;
+            M_vals(ind_start:ind_stop,1) = M';
+            ind_start = ind_stop+1;
+        end
     end
 end
-tot_inds = zeros(tot_num,7);
-M_vals = zeros(1,tot_num);
-ind_start = 1;
-for ct1=0:N
-    for ct2=0:N
-        M = calc_Mfunc(ct1,ct2,r1,r2);
-        inds_mat = mbp_ab(ct1,ct2);
-        ind_stop = ind_start+size(inds_mat,1)-1;
-        tot_inds(ind_start:ind_stop,1) = ind_start:ind_stop;
-        tot_inds(ind_start:ind_stop,2:7) = inds_mat;
-        M_vals(ind_start:ind_stop,1) = M';
-        ind_start = ind_stop+1;
-    end
-end
-end
-
-function M = calc_Mfunc(a,b,r1,r2)
-c = min(a, b);
-ax_ang1 = vrrotmat2vec(r1');
-ax_ang2 = vrrotmat2vec(r2');
-
-inds_mat = mbp_ab(a,b);
-num_inds = size(inds_mat,1);
-
-
-Na = 2*a; Nb = 2*b;
-Ua_r1 = rotation(ax_ang1(1:3), ax_ang1(4), Na);
-Ub_r2 = rotation(ax_ang2(1:3), ax_ang2(4), Nb);
-
-M = zeros(1, num_inds);
-for ct1=1:num_inds
-    g1 = inds_mat(ct1, 4);
-    a1 = inds_mat(ct1, 5);
-    b1 = inds_mat(ct1, 6);
-    g1_inda =  g1 + c + 1;
-    g1_indb = -g1 + c + 1;
-    a1_ind = a1 + a + 1;
-    b1_ind = b1 + b + 1;
-    M(ct1) = Ua_r1(a1_ind, g1_inda)*Ub_r2(b1_ind, g1_indb);
-end
-
-if a == b
-    PI_ab = sqrt(2*a+1);
-else
-    if (a > b)
-        PI_ab = sqrt(prod(2*b+1:2*a+1));
-    else
-        PI_ab = sqrt(prod(2*a+1:2*b+1));
-    end
-end
-
-M = PI_ab*M/(2*pi*pi);
 end
 
 % M = zeros(10, num_inds);
