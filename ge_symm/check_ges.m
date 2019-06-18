9a42d6c52a02d1acf7b9fb299d5848ec41fde232
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-18 13:04:02 -0400

    Checking the GES symmetry.

--- ge_symm/check_ges.m
+++ ge_symm/check_ges.m
@@ -12,14 +12,17 @@ Svec = s1.S;
 
 rots = rot_mats(:,:,1);
 
-N = [0,4];
+% N = [0,4];
+% N = 0:1;
+N = 1;
 M1 = mbp_funcs_vals(rots, N);
 
 g_ypi = vrrotvec2mat([0,1,0, pi]);
+r2 = rots(:,1:3); r1 = rots(:,1:3);
 r1_ges = g_ypi*r2; r2_ges = g_ypi*r1;
 M1_ges = mbp_funcs_vals([r1_ges, r2_ges], N);
 
-norm(M1'*Svec - M1_ges'*Svec)
+norm(M1*Svec - M1_ges*Svec)
 
 rmpath(genpath('../Util_functions/'));
 rmpath(genpath('../GB_Parameters/'));
