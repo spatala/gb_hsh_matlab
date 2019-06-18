172a95b4b62dac965b7eb5e8cf45c6cf451aa6cc
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-18 14:49:42 -0400

    Fixed error with r2 rotation matrix.

--- ge_symm/check_ges.m
+++ ge_symm/check_ges.m
@@ -13,12 +13,12 @@ Svec = s1.S;
 rots = rot_mats(:,:,1);
 
 % N = [0,4];
-% N = 0:1;
-N = 1;
+N = 0:4;
+% N = 1;
 M1 = mbp_funcs_vals(rots, N);
 
 g_ypi = vrrotvec2mat([0,1,0, pi]);
-r2 = rots(:,1:3); r1 = rots(:,1:3);
+r2 = rots(:,4:6); r1 = rots(:,1:3);
 r1_ges = g_ypi*r2; r2_ges = g_ypi*r1;
 M1_ges = mbp_funcs_vals([r1_ges, r2_ges], N);
 
