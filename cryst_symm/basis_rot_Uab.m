5d2b86fcd86e2b1c1d61529c4889b626472a69e0
Srikanth Patala <spatala@ncsu.edu>
2019-06-19 09:00:15 -0400

    Changes to basis_rot_Uab.m file. Saving the orders (a,b) for which symmetrized basis functions exist.

--- cryst_symm/basis_rot_Uab.m
+++ cryst_symm/basis_rot_Uab.m
@@ -5,10 +5,10 @@ addpath(genpath('../GB_Parameters/'));
 
 Nmax = 4;
 
-pt_grp = 'O';
-% pt_grp = 'C2';
+% pt_grp = 'O';
+pt_grp = 'C2';
 
-if (pt_grp == 'O')
+if (strcmp(pt_grp,'O'))
     num_gen = 2;
     
     a_ax1 = [0 0 1]; a_ang1 = pi/2;
@@ -24,21 +24,22 @@ if (pt_grp == 'O')
     b_ax4 = [1 0 0]; b_ang4 = pi/2;
 end
 
-if (pt_grp == 'C2')
+if (strcmp(pt_grp,'C2'))
     num_gen = 1;
     
-a_ax1 = [0 0 1]; a_ang1 = pi;
-        b_ax1 = [0 0 1]; b_ang1 = 0;
-        
-        a_ax2 = [0 0 1]; a_ang2 = 0;
-        b_ax2 = [0 0 1]; b_ang2 = pi;
+    a_ax1 = [0 0 1]; a_ang1 = pi;
+    b_ax1 = [0 0 1]; b_ang1 = 0;
+    
+    a_ax2 = [0 0 1]; a_ang2 = 0;
+    b_ax2 = [0 0 1]; b_ang2 = pi;
 end
 
+symm_orders = zeros(Nmax^2,2);
+ct3 = 1;
 for ct1=0:Nmax
     for ct2=0:Nmax
         
-        a_val = ct1; Na = 2*a_val;
-        b_val = ct2; Nb = 2*b_val;
+        a_val = ct1; Na = 2*a_val; b_val = ct2; Nb = 2*b_val;
         
         R1 = kron(rotation(a_ax1,a_ang1,Na),rotation(b_ax1,b_ang1,Nb));
         [v,d] = eig(R1);
@@ -58,34 +59,31 @@ for ct1=0:Nmax
                     [v, d] = eig(P1*Q2*P1);
                     col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
                     if any(col)
-                        X2 = orth(v(:,col)); P2 = X2*X2';
-                        R3 = kron(rotation(a_ax3,a_ang3,Na),rotation(b_ax3,b_ang3,Nb));
-                        [v,d] = eig(R3);
-                        col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
-                        if any(col)
-                            Y3 = orth(v(:,col)); Q3 = Y3*Y3';
-                            [v, d] = eig(P2*Q3*P2);
+                        if num_gen == 1
+%                             save_symm_arr(a_val, b_val, v, col);
+                            symm_orders(ct3,:) = [ct1, ct2]; ct3 = ct3 + 1;
+                        elseif num_gen == 2
+                            X2 = orth(v(:,col)); P2 = X2*X2';
+                            R3 = kron(rotation(a_ax3,a_ang3,Na),rotation(b_ax3,b_ang3,Nb));
+                            [v,d] = eig(R3);
                             col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
                             if any(col)
-                                X3 = orth(v(:,col)); P3 = X3*X3';
-                                R4 = kron(rotation(a_ax4,a_ang4,Na),rotation(b_ax4,b_ang4,Nb));
-                                [v,d] = eig(R4);
+                                Y3 = orth(v(:,col)); Q3 = Y3*Y3';
+                                [v, d] = eig(P2*Q3*P2);
                                 col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
                                 if any(col)
-                                    Y4 = orth(v(:,col)); Q4 = Y4*Y4';
-                                    [v, d] = eig(P3*Q4*P3);
+                                    X3 = orth(v(:,col)); P3 = X3*X3';
+                                    R4 = kron(rotation(a_ax4,a_ang4,Na),rotation(b_ax4,b_ang4,Nb));
+                                    [v,d] = eig(R4);
                                     col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
                                     if any(col)
-                                        
-                                        S = orth(v(:,col));
-                                        if size(S,2)>1
-                                            [ct1, ct2, size(S,2)]
+                                        Y4 = orth(v(:,col)); Q4 = Y4*Y4';
+                                        [v, d] = eig(P3*Q4*P3);
+                                        col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
+                                        if any(col)
+%                                             save_symm_arr(a_val, b_val, v, col);
+                                            symm_orders(ct3,:) = [ct1, ct2]; ct3 = ct3 + 1;
                                         end
-                                        c_val = min(a_val, b_val);
-                                        S = repmat(S, 2*c_val+1, 1);
-                                        mat_name = ['Sarr_',num2str(a_val),'_',num2str(b_val),'.mat'];
-                                        save(mat_name,'S');
-                                        
                                     end
                                 end
                             end
@@ -97,6 +95,10 @@ for ct1=0:Nmax
     end
 end
 %
+symm_orders(ct3:end,:) = [];
+
+mat_name = ['symm_a_b_',pt_grp,'.mat'];
+save(mat_name,'symm_orders');
 
 
 rmpath(genpath('../Util_functions/'));
