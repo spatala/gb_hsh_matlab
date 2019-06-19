0a609455df0aa02c1f9a1d2b9eb2a05a0df6bb12
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-19 16:07:42 -0400

    Modified save_symm_arr

--- cryst_symm/save_symm_arr.m
+++ cryst_symm/save_symm_arr.m
@@ -1,11 +1,5 @@
 function save_symm_arr(a_val, b_val, v, col, pt_grp,fname)
 S = orth(v(:,col));
-% if size(S,2)>1
-%     [ct1, ct2, size(S,2)]
-% end
-c_val = min(a_val, b_val);
-S = repmat(S, 2*c_val+1, 1);
-
 mat_name = [fname,'/ptgrp_',pt_grp,'/cryst_symm/Sarr_',...
     num2str(a_val),'_',num2str(b_val),'.mat'];
 save(mat_name,'S');
