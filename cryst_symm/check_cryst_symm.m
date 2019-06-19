5401cba750f358d9f7fb1026866871552cad82fc
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-19 12:33:54 -0400

    Check crystal symmetries with specified point group.

--- cryst_symm/check_cryst_symm.m
+++ cryst_symm/check_cryst_symm.m
@@ -3,43 +3,43 @@ clear all; clc;
 addpath(genpath('../Util_functions/'));
 addpath(genpath('../GB_Parameters/'));
 
-s1 = load('rand_gb_rots.mat');
-rot_mats = s1.rot_mats;
+s1 = load('rand_gb_rots.mat'); rot_mats = s1.rot_mats;
 
-a_val = 4; b_val = 0;
-mat_name = ['Sarr_',num2str(a_val),'_',num2str(b_val),'.mat'];
+% pt_grp = 'O';
+pt_grp = 'C2';
 
-s1 = load(mat_name);
-Svec = s1.S;
+Nmax = 4;
 
-s1 = load('O_SymmMat.mat');
-SymmMat = s1.SymmMat_O;
-
+symm_mat_name = ['../data_files/ptgrp_',pt_grp,'/SymmMat_',pt_grp,'.mat'];
+s1 = load(symm_mat_name);
+SymmMat = s1.SymmMat;
+num_symm = length(SymmMat);
 
 rots = rot_mats(:,:,1);
 r1 = rots(:,1:3); r2 = rots(:,4:6);
-
-% N = [0,4];
-% M1 = mbp_funcs_vals(rots, N);
-
-M1 = calc_Mfunc(a_val,b_val,rots);
-
-% S1 = SymmMat{4}; S2 = SymmMat{12};
-diff_vec = zeros(24*24,1);
-ct3 = 1;
-for ct1 = 1:24
-    for ct2 = 1:24
-        S1 = SymmMat{ct1}; S2 = SymmMat{ct2};
+for a_val = 0:Nmax
+    for b_val = 0:Nmax
+        mat_name = ['Sarr_',num2str(a_val),'_',num2str(b_val),'.mat'];
+        s1 = load(mat_name);
+        Svec = s1.S;
         
-        r1s = r1*S1; r2s = r2*S2;
-        M1_symm = calc_Mfunc(a_val,b_val,[r1s, r2s]);
+        M1 = calc_Mfunc(a_val,b_val,rots);
         
-        diff_vec(ct3) = norm(M1_symm*Svec - M1*Svec);
-        ct3 = ct3 + 1;
+        diff_vec = zeros(num_symm*num_symm,1);
+        ct3 = 1;
+        for ct1 = 1:num_symm
+            for ct2 = 1:num_symm
+                S1 = SymmMat{ct1}; S2 = SymmMat{ct2};
+                
+                r1s = r1*S1; r2s = r2*S2;
+                M1_symm = calc_Mfunc(a_val,b_val,[r1s, r2s]);
+                
+                diff_vec(ct3) = norm(M1_symm*Svec - M1*Svec);
+                ct3 = ct3 + 1;
+            end
+        end
+        max(diff_vec)
     end
 end
-
-max(diff_vec)
-
 rmpath(genpath('../Util_functions/'));
 rmpath(genpath('../GB_Parameters/'));
