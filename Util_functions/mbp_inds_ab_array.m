ed3479ef98e7a2cad0eddd81b7780a91a2113d3a
spatala <patala@sp-new28c.mse.ncsu.edu>
2019-06-20 14:55:08 -0400

    Changed nsymm to size(symm_orders,1).

--- Util_functions/mbp_inds_ab_array.m
+++ Util_functions/mbp_inds_ab_array.m
@@ -2,7 +2,7 @@ function tot_inds = mbp_inds_ab_array(symm_orders, num_rows)
 tot_inds = zeros(num_rows,7);
 ind_start = 1;
 
-for ct1=1:nsymm
+for ct1=1:size(symm_orders,1)
     a_val = symm_orders(ct1,1);
     b_val = symm_orders(ct1,2);
     
