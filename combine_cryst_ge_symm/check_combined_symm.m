ffba1031347e65d72c7fbc819aef6fa7897a19a0
Srikanth Patala <spatala@ncsu.edu>
2019-06-19 22:42:21 -0400

    Added point group flexibility.

--- combine_cryst_ge_symm/check_combined_symm.m
+++ combine_cryst_ge_symm/check_combined_symm.m
@@ -1,36 +1,45 @@
 clear all; clc;
 
-addpath(genpath('./Util_functions/'));
-addpath(genpath('./GB_Parameters/'));
+addpath(genpath('../Util_functions/'));
+addpath(genpath('../GB_Parameters/'));
+
+Nmax = 4;
+
+fname = get_dir_name();
+
+% pt_grp = 'O';
+pt_grp = 'C2';
+
+mat_name = [fname,'/ptgrp_',pt_grp,'/cryst_symm/symm_a_b_',pt_grp,'.mat'];
+s1 = load(mat_name);
+symm_orders = s1.symm_orders;
 
 
 s1 = load('rand_gb_rots.mat');
 rot_mats = s1.rot_mats;
 
-s1 = load('Sarr_C2_4_4_combined.mat');
-Svec = s1.S;
 
 rots = rot_mats(:,:,1);
 r1 = rots(:,1:3); r2 = rots(:,4:6);
 
-a_val = 4; b_val = 4;
+
+
+M1 = calc_Mfunc_ab_array(symm_orders, rots);
+
+% a_val = 4; b_val = 4;
 % M1 = mbp_funcs_vals(rots, N);
-M1 = calc_Mfunc(a_val,b_val,rots);
-
-
+% M1 = calc_Mfunc(a_val,b_val,rots);
 % norm(M1*Svec - M1_ges*Svec)
 
+mat_name = [fname,'/ptgrp_',pt_grp,'/combined_symm/Sarr_combined_Nmax_',...
+    num2str(Nmax),'.mat'];
+s1 = load(mat_name);
+Svec = s1.S;
 
-fname = get_dir_name();
-pt_grp = 'C2';
 symm_mat_name = [fname,'/ptgrp_',pt_grp,'/SymmMat_',pt_grp,'.mat'];
 s1 = load(symm_mat_name);
 SymmMat = s1.SymmMat;
 num_symm = length(SymmMat);
-
-% s1 = load('data_files/ptgrp_C2/cryst_symm/Sarr_4_4.mat');
-% Svec = s1.S;
-
 
 diff_vec = zeros(num_symm*num_symm*2,1);
 ct3 = 1;
@@ -39,15 +48,17 @@ for ct1 = 1:num_symm
         S1 = SymmMat{ct1}; S2 = SymmMat{ct2};
 
         r1s = r1*S1; r2s = r2*S2;
-        M1_symm = calc_Mfunc(a_val,b_val,[r1s, r2s]);
+        rots_symm = [r1s, r2s];
+        M1_symm = calc_Mfunc_ab_array(symm_orders, rots_symm);
 
         diff_vec(ct3) = norm(M1_symm*Svec - M1*Svec);
         ct3 = ct3 + 1;
         
         g_ypi = vrrotvec2mat([0,1,0, pi]);
         r1_ges = g_ypi*r2s; r2_ges = g_ypi*r1s;
-        M1_ges = calc_Mfunc(a_val,b_val,[r1_ges, r2_ges]);
-        diff_vec(ct3) = norm(M1_symm*Svec - M1*Svec);
+        rots_ges = [r1_ges, r2_ges];
+        M1_ges = calc_Mfunc_ab_array(symm_orders, rots_ges);
+        diff_vec(ct3) = norm(M1_ges*Svec - M1*Svec);
         ct3 = ct3 + 1;
     end
 end
