ba1982eea81148ed3365c6af44ade1104d27bdb8
Srikanth Patala <spatala@ncsu.edu>
2019-06-19 22:42:45 -0400

    Calculate indices for (a,b) array of values.

---
+++ Util_functions/mbp_inds_ab_array.m
@@ -0,0 +1,15 @@
+function tot_inds = mbp_inds_ab_array(symm_orders, num_rows)
+tot_inds = zeros(num_rows,7);
+ind_start = 1;
+
+for ct1=1:nsymm
+    a_val = symm_orders(ct1,1);
+    b_val = symm_orders(ct1,2);
+    
+    inds_mat = mbp_ab(a_val,b_val);
+    ind_stop = ind_start+size(inds_mat,1)-1;
+    tot_inds(ind_start:ind_stop,1) = ind_start:ind_stop;
+    tot_inds(ind_start:ind_stop,2:7) = inds_mat;
+    ind_start = ind_stop+1;
+end
+end
