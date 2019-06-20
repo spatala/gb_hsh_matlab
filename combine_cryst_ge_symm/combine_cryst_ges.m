a2524dbd20da9f6c82cfbd6fd4c672aa628c6b17
Srikanth Patala <spatala@ncsu.edu>
2019-06-19 22:45:06 -0400

    Modified file to combine crystal and GE symmetries for all possible (a,b) values.

--- combine_cryst_ge_symm/combine_cryst_ges.m
+++ combine_cryst_ge_symm/combine_cryst_ges.m
@@ -1,14 +1,19 @@
 clear all; clc;
 
+addpath(genpath('../Util_functions/'));
+addpath(genpath('../GB_Parameters/'));
 
-s1 = load('data_files/ptgrp_C2/cryst_symm/Sarr_4_4.mat');
-% X0 = sparse(s1.Sarr_nmax);
-% X0 = s1.Sarr_nmax;
-X0 = s1.S;
+Nmax = 4;
+fname = get_dir_name();
+pt_grp = 'C2';
+
+s1 = load([fname,'/ptgrp_',pt_grp,'/cryst_symm/Sarr_Nmax_',...
+    num2str(Nmax),'.mat']);
+X0 = s1.Sarr_nmax;
 P0 = X0*X0';
 
-s1 = load('ge_symm/Y_ges.mat');
-% Y1 = sparse(s1.S);
+s1 = load([fname,'/ptgrp_',pt_grp,'/ge_symm/Y_ges_Nmax_',...
+    num2str(Nmax),'.mat']);
 Y1 = s1.col1;
 Y1 = orth(Y1);
 Q1 = Y1*Y1';
@@ -19,6 +24,11 @@ col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
 if any(col)
     % display(v(:,col));
     S = orth(v(:,col));
-    mat_name = 'Sarr_C2_4_4_combined.mat';
+    mat_name = [fname,'/ptgrp_',pt_grp,'/combined_symm/Sarr_combined_Nmax_',...
+    num2str(Nmax),'.mat'];
+%     mat_name = 'Sarr_C2_4_4_combined.mat';
     save(mat_name,'S');
 end
+
+rmpath(genpath('../Util_functions/'));
+rmpath(genpath('../GB_Parameters/'));
