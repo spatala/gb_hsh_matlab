3e3052ebef0f6ed9f60bc2cf1845318855e662c6
Srikanth Patala <spatala@ncsu.edu>
2019-06-20 21:18:42 -0400

    Modified code for taking eigen-vectors of GES for combining symmetries.

--- combine_cryst_ge_symm/combine_cryst_ges.m
+++ combine_cryst_ge_symm/combine_cryst_ges.m
@@ -3,30 +3,49 @@ clear all; clc;
 addpath(genpath('../Util_functions/'));
 addpath(genpath('../GB_Parameters/'));
 
-Nmax = 4;
 fname = get_dir_name();
-pt_grp = 'C2';
+% pt_grp = 'C2';
+pt_grp = 'O';
+Nmax = 8;
+
+mat_name = [fname,'/ptgrp_',pt_grp,'/cryst_symm/symm_ab_',...
+    pt_grp,'_Nmax_',num2str(Nmax),'.mat'];
+s1 = load(mat_name);
+symm_orders = s1.symm_orders;
+nsymm = size(symm_orders,1);
+a1 = symm_orders(:,1); b1 = symm_orders(:,2); c1 = min(a1, b1);
+num_rows = sum((2*a1+1).*(2*b1+1).*(2*c1+1));
+Nmax = max(a1);
 
 s1 = load([fname,'/ptgrp_',pt_grp,'/cryst_symm/Sarr_Nmax_',...
     num2str(Nmax),'.mat']);
-X0 = s1.Sarr_nmax;
+X0 = s1.S;
+for ct1=1:size(X0,2)
+    ind1 = find(sqrt(real(X0(:,ct1)).^2 + imag(X0(:,ct1)).^2) < 1e-10);
+    X0(ind1,ct1) = 0;
+end
+X0 = sparse(X0);
 P0 = X0*X0';
 
-s1 = load([fname,'/ptgrp_',pt_grp,'/ge_symm/Y_ges_Nmax_',...
+% s1 = load([fname,'/ptgrp_',pt_grp,'/ge_symm/Y_ges_Nmax_',...
+%     num2str(Nmax),'.mat']);
+% Y1 = sparse(s1.col1);
+% Y1 = orth(Y1);
+
+s1 = load([fname,'/ptgrp_',pt_grp,'/ge_symm/Sarr_ges_Nmax_',...
     num2str(Nmax),'.mat']);
-Y1 = s1.col1;
-Y1 = orth(Y1);
+Y1 = s1.S;
 Q1 = Y1*Y1';
 
-[v, d] = eig(P0*Q1*P0);
+eig_mat = P0*Q1*P0;
+
+[v, d] = eig(full(eig_mat));
 col = (abs(imag(diag(d)))<1e-5 & abs(real(diag(d))-1)<1e-5);
             
 if any(col)
-    % display(v(:,col));
     S = orth(v(:,col));
     mat_name = [fname,'/ptgrp_',pt_grp,'/combined_symm/Sarr_combined_Nmax_',...
     num2str(Nmax),'.mat'];
-%     mat_name = 'Sarr_C2_4_4_combined.mat';
     save(mat_name,'S');
 end
 
