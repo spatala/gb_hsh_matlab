e1dc2cb87fc48bdf6b608569ad2cc522eace824e
Srikanth Patala <spatala@ncsu.edu>
2019-06-27 20:57:53 -0400

    Plotting routines.

--- Util_functions/set_vars.m
+++ Util_functions/set_vars.m
@@ -1,7 +1,8 @@
 function s1 = set_vars()
-s1.Nmax = 4; 
+
 
 %%% Point Group Settings
-s1.pt_grp = 'C2';
-% s1.pt_grp = 'O';
+% s1.pt_grp = 'C2'; s1.Nmax = 4; 
+s1.pt_grp = 'O'; s1.Nmax = 6; 
+% s1.pt_grp = 'D2';
 end
