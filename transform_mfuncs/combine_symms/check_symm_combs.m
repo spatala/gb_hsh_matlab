7cf59fb0ddc4b5b99e3229f2cbcff9495238dec1
Srikanth Patala <spatala@ncsu.edu>
2019-06-27 21:03:03 -0400

    Codes for combine crystal and ge symmetries.

--- transform_mfuncs/combine_symms/check_symm_combs.m
+++ transform_mfuncs/combine_symms/check_symm_combs.m
@@ -17,65 +17,42 @@ addpath(genpath(util_dir));
 s1 = set_vars();
 Nmax = s1.Nmax; pt_grp = s1.pt_grp;
 
-data_fname0 = [top_dir,'data_files/ptgrp_',pt_grp,'/cryst_symm/'];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data_fname = [top_dir,'data_files/ptgrp_',pt_grp,'/'];
+data_fname0 = [data_fname,'ge_symm/'];
+data_fname1 = [data_fname,'cryst_symm/'];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-symm_orders = zeros(Nmax^2,2);
-ct3 = 1;
-for ct1=0:Nmax
-    for ct2=0:Nmax
-        symm_orders(ct3,:) = [ct1,ct2];
-        ct3 = ct3 + 1;
-    end
-end
-a1 = symm_orders(:,1); b1 = symm_orders(:,2); c1 = min(a1,b1);
-num_cols = sum((2*a1+1).*(2*b1+1).*(2*c1+1));
-nsymm = size(symm_orders,1);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-s1 = load([top_dir,'GB_Parameters/rand_gb_rots.mat']); rot_mats = s1.rot_mats;
-rots1 = rot_mats(:,:,floor(size(rot_mats,3)*rand()));
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% s1 = load([top_dir,'GB_Parameters/rand_gb_rots.mat']); rot_mats = s1.rot_mats;
+% rots1 = rot_mats(:,:,floor(size(rot_mats,3)*rand()));
+% g1 = rots1(:,1:3); g2 = rots1(:,4:6);
+M1 = vrrotvec2mat([1,1,1,pi/3]);
+th = rand()*pi; ph = rand()*2*pi;
+nvec = [sin(th)*cos(ph); sin(th)*sin(ph); cos(th)];
+rots1 = mbp_to_rots([M1, nvec]);
 g1 = rots1(:,1:3); g2 = rots1(:,4:6);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Zpi = vrrotvec2mat([0,0,1,pi]);
-
-% %%%%
-% gs1 = eye(3); gs2 = eye(3);
-% Sg1 = g1*gs1; Sg2 = g2*gs2;
-
-% %%%%
-% gs1 = Zpi; gs2 = eye(3);
-% Sg1 = g1*gs1; Sg2 = g2*gs2;
-
-% %%%%
-% gs1 = eye(3); gs2 = Zpi;
-% Sg1 = g1*gs1; Sg2 = g2*gs2;
-% 
-
-% %%%%
-% gs1 = Zpi; gs2 = Zpi;
-% Sg1 = g1*gs1; Sg2 = g2*gs2;
-
-%%%%
-g_ypi = vrrotvec2mat([0,1,0,pi]);
-% Sg1 = g_ypi*g2; Sg2 = g_ypi*g1;
-% Sg1 = g_ypi*g2*Zpi; Sg2 = g_ypi*g1;
-% Sg1 = g_ypi*g2;     Sg2 = g_ypi*g1*Zpi;
-Sg1 = g_ypi*g2*Zpi; Sg2 = g_ypi*g1*Zpi;
+mat_name = [data_fname,'symm_ab_',pt_grp,'_Nmax_',num2str(Nmax),'.mat'];
+s1 = load(mat_name); symm_orders = s1.symm_orders;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+symm_rots = get_symm_rots(g1,g2, pt_grp, data_fname);
 
-Mvec_12_nmax = zeros(1,num_cols); SMvec_12_nmax = zeros(1,num_cols);
-Mvec_ab_12  = calc_Mvec( g1,  g2, symm_orders);
-SMvec_ab_12 = calc_Mvec(Sg1, Sg2, symm_orders);
-
-mat_name = [data_fname0,'Sarr_combined_',num2str(Nmax),'.mat'];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mat_name = [data_fname,'Sarr_combined_nmax_',num2str(Nmax),'.mat'];
 % mat_name = [data_fname0,'Sarr_Nmax_',num2str(Nmax),'.mat'];
 s1 = load(mat_name); S = (s1.S);
-size(orth(S))
+% size(orth(S))
+nsymm = size(symm_rots,3);
 
-norm(Mvec_12_nmax*S - SMvec_12_nmax*S)
+diff_arr = zeros(nsymm,1);
+for ct1=1:nsymm
+    rots = symm_rots(:,:,ct1);
+    Sg1 = rots(:,1:3); Sg2 = rots(:,4:6);
+    
+    Mvec  = calc_Mvec( g1,  g2, symm_orders);
+    SMvec = calc_Mvec(Sg1, Sg2, symm_orders);
+    diff_arr(ct1) = norm(Mvec*S - SMvec*S);
+end
 
 rmpath(genpath(util_dir));
