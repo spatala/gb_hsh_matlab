7fa9396ba541b0d66ac32a38a8d09a373416838e
Srikanth Patala <spatala@ncsu.edu>
2019-06-27 21:03:03 -0400

    Codes for combine crystal and ge symmetries.

--- transform_mfuncs/combine_symms/generate_symm_combs.m
+++ transform_mfuncs/combine_symms/generate_symm_combs.m
@@ -16,15 +16,17 @@ addpath(genpath(util_dir));
 s1 = set_vars();
 Nmax = s1.Nmax; pt_grp = s1.pt_grp;
 
-data_fname0 = [top_dir,'data_files/ptgrp_',pt_grp,'/cryst_symm/'];
+data_fname = [top_dir,'data_files/ptgrp_',pt_grp,'/'];
+data_fname0 = [data_fname,'ge_symm/'];
+data_fname1 = [data_fname,'cryst_symm/'];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%% Load Grain Exchange Symmetry eigen vectors
-mat_name = [data_fname0,'ges_nmax_',num2str(Nmax),'.mat'];
+mat_name = [data_fname0,'ges_full_symm_orders_nmax_',num2str(Nmax),'.mat'];
 s1 = load(mat_name); S_ges = s1.S;
 
 %%%% Load Crystal Symmetry eigen vectors
-mat_name = [data_fname0,'Sarr_full_symm_orders.mat'];
+mat_name = [data_fname1,'Sarr_full_symm_orders_nmax_',num2str(Nmax),'.mat'];
 s1 = load(mat_name); S_csymm = s1.S;
 
 %%%% Combine orthogonal-eigen-vectors
@@ -33,7 +35,7 @@ col1 = (abs(imag(diag(d1)))<1e-5 & abs(real(diag(d1))-1)<1e-5);
 
 %%%% Save combined (crystal-symmetry and GES) S-vectors
 if any(col1)
-    save_symm_arr(Nmax, v1, col1, data_fname0);
+    save_symm_arr(Nmax, v1, col1, data_fname);
 end
 
 end
@@ -46,6 +48,6 @@ end
 
 function save_symm_arr(Nmax, v, col, fname)
 S = orth(v(:,col));
-mat_name = [fname,'Sarr_combined_',num2str(Nmax),'.mat'];
+mat_name = [fname,'Sarr_combined_nmax_',num2str(Nmax),'.mat'];
 save(mat_name,'S');
 end
