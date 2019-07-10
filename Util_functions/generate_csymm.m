5d2c2f242997d2f963595f29c624f5d3080cdf8d
spatala <srikanth.patala@gmail.com>
2019-07-10 14:02:28 -0400

    Added line to remove small values from Mrot matrix.

--- Util_functions/generate_csymm.m
+++ Util_functions/generate_csymm.m
@@ -1,4 +1,4 @@
-function csymm_mat = generate_csymm(gs1, gs2, symm_orders)
+% function csymm_mat = generate_csymm(gs1, gs2, symm_orders)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Function to compute the full Symmetry matrix that operates on
 %%%% M(0,0)....M(Nmax, Nmax) given by symm_orders
@@ -24,6 +24,7 @@ nsymm = size(symm_orders,1);
 csymm_mat = sparse(num_cols,num_cols);
 ind_start = 1;
 for ct1 = 1:nsymm
+%     nsymm
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     a_val = symm_orders(ct1,1); b_val = symm_orders(ct1,2);
     c_val = min(a_val, b_val);
@@ -34,8 +35,12 @@ for ct1 = 1:nsymm
     Mrot = kron(eye(Nc+1), trRr_ab_12);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     ind_stop = ind_start + nsz1 - 1;
+    
+    %%%% Mrot is a complex matrix
+    %     Mrot = adjust_complex_mats(Mrot, 1e-14);
     csymm_mat(ind_start:ind_stop, ind_start:ind_stop) = Mrot;
     ind_start = ind_stop + 1;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 end
+
