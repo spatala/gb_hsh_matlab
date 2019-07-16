4642f5530989a9858f17545873a5d927b87d2662
spatala <srikanth.patala@gmail.com>
2019-07-16 14:12:19 -0400

    Added Sarr files.

--- Plotting_Routines/plot_bpl_funcs/plot_Mab_bplanes.m
+++ Plotting_Routines/plot_bpl_funcs/plot_Mab_bplanes.m
@@ -1,6 +1,5 @@
 clear all; clc;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 curr_pwd = split(pwd,'/');
 top_dir = '';
 for ct1=1:length(curr_pwd)
@@ -12,18 +11,21 @@ end
 util_dir = strcat(top_dir,'Util_functions','/');
 addpath(genpath(util_dir));
 
-s1 = set_vars();
-Nmax = s1.Nmax; pt_grp = s1.pt_grp;
+s1 = set_vars(); Nmax = s1.Nmax; pt_grp = s1.pt_grp;
 
-data_fname = [top_dir,'data_files/ptgrp_',pt_grp,'/'];
-data_fname0 = [data_fname,'ge_symm/'];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mat_name = [data_fname,'symm_ab_',pt_grp,'_Nmax_',num2str(Nmax),'.mat'];
+data_fname = [top_dir,'data_files/ptgrp_',pt_grp,'/'];
+data_fname0 = [data_fname,'nmax_',num2str(Nmax),'/'];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mat_name = [data_fname0,'symm_ab_',pt_grp,'_Nmax_',num2str(Nmax),'.mat'];
 s1 = load(mat_name); symm_orders = s1.symm_orders;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% pts = gen_sph_pts(10);
-% 
+
+% pts = gen_sph_pts(25);
+
 % %%%%%% Rotate points
 % %%% Z-axis goes to [1,1,1]
 % zrot = [1,1,1]'; zrot = zrot/norm(zrot);
@@ -38,13 +40,13 @@ s1 = load(mat_name); symm_orders = s1.symm_orders;
 % x1 = rot_pts(:,1); y1 = rot_pts(:,1); z1 = rot_pts(:,1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-num = 100;
+num = 25;
 [X, Y, Z] = sphere(num);
 x1 = X(:); y1 = Y(:); z1 = Z(:);
 
-% m1 = vrrotvec2mat([1,1,1, pi/3]);
+m1 = vrrotvec2mat([1,1,1, pi/3]);
 % m1 = vrrotvec2mat([1,0,0, pi/4]);
-m1 = vrrotvec2mat([1,0,0, 0]);
+% m1 = vrrotvec2mat([1,0,0, 0]);
 % q1 = [cos(pi/4), 0, 0, sin(pi/4)];
 % m1 = quat2mat(q1);
 nsz = (num+1)^2;
@@ -60,7 +62,8 @@ end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mat_name = [data_fname,'Sarr_combined_nmax_',num2str(Nmax),'.mat'];
+mat_name = [data_fname0, ...
+    'Sarr_cryst_ges_nmax_',num2str(Nmax),'.mat'];
 s1 = load(mat_name); S = (s1.S);
 num_cols = size(S,1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -69,7 +72,8 @@ for ct1=1:nsz
     ct1
     rots1 = rots(:,:,ct1);
     g1 = rots1(:,1:3); g2 = rots1(:,4:6);
-    Mvec(ct1,:) = calc_Mvec(g1,g2,symm_orders);
+    tMvec = calc_Mvec(g1,g2,symm_orders);
+    Mvec(ct1,:) = tMvec;
 end
 
 symm_Mvec = Mvec*S;
