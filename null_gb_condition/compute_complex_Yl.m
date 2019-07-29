b9d5454eeb2c460afcc2b059b40f4ec0f777dd10
spatala <srikanth.patala@gmail.com>
2019-07-29 13:51:09 -0400

    Indentation.

--- null_gb_condition/compute_complex_Yl.m
+++ null_gb_condition/compute_complex_Yl.m
@@ -14,40 +14,40 @@ Yl_vals = zeros(2*l+1,1);
 
 ct1 = 1;
 for m = -l:l
-%%%%% Dependent on $m$
-% check if m is odd and negative
-isoddm = mod(m,2) == 1;
-isnegm = m < 0;
-% if m is negative, compute the symmetric function where m > 0
-if isnegm
-    m = abs(m);
-end
-
-% normalization
-b = 1/prod(l-m+1:l+m); % factorial(n-m)/factorial(n+m);
-C = sqrt(a*b);
-
-Pm = P(abs(m)+1,:)';
-E = exp(1i*m*phi);
-
-% surface spherical harmonics
-Y = C * Pm .* E;
-
-% include Condon-Shortley phase term
-% this term is in fact included in MATLAB's associated Legendre function
-% definition, so what we are doing here is removing it if not requested
-if ~CS_phase && isoddm
-    Y = -Y;
-end
-
-% if m was negative
-if isnegm
-    Y = conj(Y);
-    if isoddm
+    %%%%% Dependent on $m$
+    % check if m is odd and negative
+    isoddm = mod(m,2) == 1;
+    isnegm = m < 0;
+    % if m is negative, compute the symmetric function where m > 0
+    if isnegm
+        m = abs(m);
+    end
+    
+    % normalization
+    b = 1/prod(l-m+1:l+m); % factorial(n-m)/factorial(n+m);
+    C = sqrt(a*b);
+    
+    Pm = P(abs(m)+1,:)';
+    E = exp(1i*m*phi);
+    
+    % surface spherical harmonics
+    Y = C * Pm .* E;
+    
+    % include Condon-Shortley phase term
+    % this term is in fact included in MATLAB's associated Legendre function
+    % definition, so what we are doing here is removing it if not requested
+    if ~CS_phase && isoddm
         Y = -Y;
     end
-end
-Yl_vals(ct1) = Y;
-ct1 = ct1 + 1;
+    
+    % if m was negative
+    if isnegm
+        Y = conj(Y);
+        if isoddm
+            Y = -Y;
+        end
+    end
+    Yl_vals(ct1) = Y;
+    ct1 = ct1 + 1;
 end
 end
