2081ba445e90e219a6d54074f523b677d71c2396
spatala <srikanth.patala@gmail.com>
2019-07-29 14:09:41 -0400

    Change PI_ab

--- null_gb_condition/get_PIab.m
+++ null_gb_condition/get_PIab.m
@@ -1,12 +1,14 @@
 function PI_ab = get_PIab(a,b)
-if a == b
-    PI_ab = sqrt(2*a+1);
-else
-    if (a > b)
-        PI_ab = sqrt(prod(2*b+1:2*a+1));
-    else
-        PI_ab = sqrt(prod(2*a+1:2*b+1));
-    end
-end
+% if a == b
+%     PI_ab = sqrt(2*a+1);
+% else
+%     if (a > b)
+%         PI_ab = sqrt(prod(2*b+1:2*a+1));
+%     else
+%         PI_ab = sqrt(prod(2*a+1:2*b+1));
+%     end
+% end
+
+PI_ab = (sqrt(2*a+1))*(sqrt(2*b+1));
 
 end
