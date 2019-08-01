688ac59df4f14e87aa6ff5c9dc7913916514e073
spatala <srikanth.patala@gmail.com>
2019-08-01 13:45:56 -0400

    Moved files around.

--- computations/other/test_rotation_mat.m
+++ computations/other/test_rotation_mat.m
@@ -6,7 +6,7 @@ w  = 8 * pi * (rand() - 0.5);
 th = pi * rand();
 ph = 2 * pi * rand();
 
-j = 1;
+j = 3;
 
 % Explicit form for j = 1
 
@@ -21,6 +21,7 @@ j = 1;
 %        (cos(w / 2) + 1i * sin(w / 2) * cos(th))^2];
 % ref = flipud(fliplr(ref));
 
+% U = rotation_mat(j, [w-2*pi, th, ph]);
 U = rotation_mat(j, w-2*pi, th, ph);
 
 R = rotation([cos(ph) * sin(th), sin(ph) * sin(th), cos(th)], w, 2 * j);
@@ -30,6 +31,7 @@ disp(['Max U R difference: ', num2str(max(abs(U(:) - R(:))))]);
 
 tic;
 for a = 1:1000
+%     U = rotation_mat(j, [w, th, ph]);
     U = rotation_mat(j, w, th, ph);
 end
 toc;
