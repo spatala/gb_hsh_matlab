33b4eb596e1d2212e4bc3f875528cc9da9e5be84
spatala <srikanth.patala@gmail.com>
2019-08-02 10:56:08 -0400

    Codes for plotting boundary-plane distributions.

--- Plotting_Routines/plot_bpl_funcs/create_Mvecs.m
+++ Plotting_Routines/plot_bpl_funcs/create_Mvecs.m
@@ -11,11 +11,11 @@ end
 util_dir = strcat(top_dir,'Util_functions','/');
 addpath(genpath(util_dir));
 
-pt_grp = 'Oh'; Nmax = 6;
+pt_grp = 'Oh'; Nmax = 8;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-m1 = vrrotvec2mat([1,1,1, pi/3]);
-% m1 = vrrotvec2mat([1,0,0, pi/4]);
+% m1 = vrrotvec2mat([1,1,1, pi/3]);
+m1 = vrrotvec2mat([1,0,0, pi/4]);
 % m1 = vrrotvec2mat([1,0,0, 0]);
 % q1 = [cos(pi/4), 0, 0, sin(pi/4)];
 % m1 = quat2mat(q1);
@@ -33,40 +33,67 @@ s1 = load(mat_name); symm_orders = s1.symm_orders;
 tot_inds = mbp_inds_ab_array(symm_orders);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-mat_name = ['Mvecs_',num2str(num),'.mat'];
-symm_num = 14;
+
 nrots = size(rots,3);
 
 a_val = symm_orders(:,1)'; b_val = symm_orders(:,2)';
 c_val = min(a_val, b_val);
 num_cols = sum((2*a_val+1).*(2*b_val+1).*(2*c_val+1));
 
+q1 = zeros(nrots,1); Q1  = zeros(nrots,3);
+a1 = zeros(nrots,1); lb1 = zeros(nrots,1);
+q2 = zeros(nrots,1); Q2  = zeros(nrots,3);
+a2 = zeros(nrots,1); lb2 = zeros(nrots,1);
 
-Mvecs = zeros(nrots,num_cols);
+for ct1 = 1:nrots
+    g1_1 = rots(:,1:3,ct1);  axang1 = vrrotmat2vec(g1_1');
+    ax1 = axang1(1:3); w_1 = axang1(4);
+    
+    q1(ct1,1) = cos(w_1/2); Q1(ct1,:) = ax1*sin(w_1/2);
+    a1(ct1,1) = q1(ct1,1)-1i*Q1(ct1,3); lb1(ct1,1) = -Q1(ct1,2)-1i*Q1(ct1,1);
+    
+    g1_2 = rots(:,4:6,ct1); axang2 = vrrotmat2vec(g1_2');
+    ax2 = axang2(1:3); w_2 = axang2(4);
 
-for ct1=1:nrots
-    ct1
-    g1_1 = rots(:,1:3,ct1); g1_2 = rots(:,4:6,ct1);
-    ma1 = rots_to_angs(g1_1, g1_2);
-    
-    
-    Mvec1 = zeros(1,num_cols);
-    for a=a_val
-        for b=b_val
-            M1 = mbp_basis(a, b, [ma1(1), ma1(2), ma1(3), ma1(4), ma1(5)]);
-            
-            cond1 = tot_inds(:,3)==a & tot_inds(:,4)==b;
-            ind_start = find(cond1,1);
-            ind_stop  = find(cond1,1,'last');
-            
-            Mvec1(ind_start:ind_stop) = M1;
-        end
-    end
-    Mvecs(ct1,:) = Mvec1;
+    q2(ct1,1) = cos(w_2/2); Q2(ct1,:) = ax2*sin(w_2/2);
+    a2(ct1,1) = q2(ct1,1)-1i*Q2(ct1,3); lb2(ct1,1) = -Q2(ct1,2)-1i*Q2(ct1,1);
 end
-Mvecs1 = sparse(Mvecs);
 
-save(mat_name, 'Mvecs1', '-v7.3');
+s = struct();
+s.q1 = q1; s.Q1 = Q1; s.a1 = a1; s.lb1 = lb1;
+s.q2 = q2; s.Q2 = Q2; s.a2 = a2; s.lb2 = lb2;
+
+mat_name = [data_fname0,'Sarr_MabInds_nmax_',num2str(Nmax),'.mat'];
+s1 = load(mat_name);
+
+tot_Uprops = s1.tot_Uprops;
+vec_inds = tot_inds(unique(tot_Uprops(:,1)),:);
+
+num_rows = size(S,1);
+% tic;
+Mvec = compute_Mvec_v1(s,nrots, num_rows, vec_inds);
+% toc;
+
+mat_name = ['Mvec_',pt_grp,'_Nmax_',num2str(Nmax),'_num_',num2str(num),'.mat'];
+save(mat_name, 'Mvec','m1','-v7.3');
+
+% Mdiff = (Mvecs(:,vec_inds(:,1)) - Mvec2(:,vec_inds(:,1)));
+% 
+% num_v = 0;
+% for ct1 = 1:nrots
+%     if norm(Mdiff(ct1,:))>1e-10
+%         num_v = num_v + 1;
+%     end
+% end
+% a1_rots  = s.a1;  a2_rots  = s.a2;
+% lb1_rots = s.lb1; lb2_rots = s.lb2;
+% q1_rots  = s.q1;  q2_rots  = s.q2;
+% Q1_rots  = s.Q1;  Q2_rots  = s.Q2;
+
+
+% Mvecs1 = sparse(Mvecs);
+
+% save(mat_name, 'Mvecs1', '-v7.3');
 % symm_Mvec = Mvecs*S(:,symm_num);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
