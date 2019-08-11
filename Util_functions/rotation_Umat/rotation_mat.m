26a35cac77cd247e7f2851de1d83af327bf85f44
Srikanth Patala <spatala@ncsu.edu>
2019-08-11 18:26:57 -0400

    Moved Plotting routines out of the folder. Indented rotation_mat code.

--- Util_functions/rotation_Umat/rotation_mat.m
+++ Util_functions/rotation_Umat/rotation_mat.m
@@ -2,15 +2,15 @@ function [U] = rotation_mat(j, rot_angs)
 % [U] = rotation_mat(j, w, th, ph) - returns the rotation matrix U of the
 %   order j, with a rotation angle w and rotation axis given by polar and
 %   aximuthal angles th and ph.
-%   
+%
 %   Follows Eq. 6 on page 81 of D. A. Varshalovich et al, Quantum Theory of
 %   Angular Momentum, 1988.
 
 
-    w = rot_angs(1); th = rot_angs(2); ph = rot_angs(3);
-    if (abs(mod(w,2*pi)) < 1e-14)
-        U = eye(2*j+1,2*j+1);
-    else
+w = rot_angs(1); th = rot_angs(2); ph = rot_angs(3);
+if (abs(mod(w,2*pi)) < 1e-14)
+    U = eye(2*j+1,2*j+1);
+else
     tmp = tan(w / 2.) * cos(th);
     tmp = (1. - 1i * tmp) / realsqrt(1 + tmp^2.);
     r_base = 1i * exp(-1i * ph) * tmp;
@@ -27,5 +27,5 @@ function [U] = rotation_mat(j, rot_angs)
         U(a, :) = U(a, :) * r_base^m(a);
         U(:, a) = U(:, a) * c_base^m(a);
     end
-    end
+end
 end
